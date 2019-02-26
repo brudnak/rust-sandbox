35e01cd1dc7e73e4d6ab669ac592aa7ad43f1d8c
Andrew Brudnak <brudnak@icloud.com>
2019-02-25 22:50:50 -0700

    Created vars.rs file.
    Learning about how variables work in the Rust programming language.
    Variables are immutable by default which means unchanging over time or
    unable to be changed unless the mut is typed after let.

---
+++ src/vars.rs
@@ -0,0 +1,19 @@
+// Variables hold primitive data or references to data
+// Variables are immutable by default
+// Rust is a block-scoped language
+
+pub fn run() {
+    let name = "Andrew";
+    let mut age = 29;
+    println!("My name is {} and I am {}", name, age);
+    age = 30;
+    println!("My name is {} and I am {}", name, age);
+
+    // Define constant
+    const ID: i32 = 001;
+    println!("ID: {}", ID);
+
+    // Assign multiple vars
+    let ( my_name, my_age ) = ("Andrew", 29);
+    println!("{} is {}", my_name, my_age);
+}
